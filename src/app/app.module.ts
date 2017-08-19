fbca8e4cd873629c52566b76d648acacd553e164
Allison Rodriguez <allisonhope10@gmail.com>
2017-08-20 00:09:13 +0200

    create a thread list item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { ThreadListComponent } from "./components/thread-list/thread-list.component";
+import { ThreadListItemComponent } from "./components/thread-list-item/thread-list-item.component";
 import { MessageService } from "./services/message-service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
     ThreadListComponent,
+    ThreadListItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/thread-list-item/thread-list-item.component.ts
@@ -0,0 +1,21 @@
+import { Message } from './../../services/message-service';
+import { Component, OnInit, Input } from "@angular/core";
+import * as moment from "moment";
+
+@Component({
+  selector: "thread-list-item",
+  templateUrl: "./thread-list-item.component.html",
+  styleUrls: ["./thread-list-item.component.scss"]
+})
+export class ThreadListItemComponent implements OnInit {
+
+  @Input() message: Message;
+  formattedTime: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.formattedTime = moment(this.message.time_sent).format("ddd D MMM, kk:mm");
+  }
+
+}
